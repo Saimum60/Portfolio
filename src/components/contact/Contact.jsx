98a89d31e3f873b7e621a3ffba52442a62012bec
saimum60 <saimum268@gmail.com>
2026-02-09 13:18:46 +0600

    done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      
+   <section className='px-4 py-10'>
+    <div className='container mx-auto flex justify-between items-center'>
+        <div>
+            <div>
+                <input type="text" name='text' placeholder='Name' className='focus:outline-none' />
+            </div>
+
+        </div>
     </div>
+   </section>
   )
 }
 
